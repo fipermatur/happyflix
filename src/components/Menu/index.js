68b356829c0d783f738612424ca9e389cf8eb714
Filipe Maturano <fipermatur@hotmail.com.br>
2020-08-03 23:50:15 -0300

    Commit Inicial

---
+++ src/components/Menu/index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Logo from '../../assets/img/Logo.png'
+import './menu.css'
+import Button from "../Button"
+
+// import ButtonLink from './components/ButtonLink'
+
+function Menu() {
+    return (
+        <nav className="Menu">
+            <a href="/">
+                <img className="Logo" src={Logo} alt="Logo da HappyFlix" />
+            </a>
+            
+            <Button as="a" className="ButtonLink" href="/">
+                + NOVO VÍDEO
+            </Button>
+        </nav>
+    )   
+}
+
+export default Menu
